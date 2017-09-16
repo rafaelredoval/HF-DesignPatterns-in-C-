a97d01fa36d5a2fe00b04142854765be0c501bf9
Terminal 04 <rafael.redoval@gmail.com>
2017-09-16 00:48:30 -0300

    first commit

---
+++ HFDesignPatterns/HFDesignPatterns/FlyBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HFDesignPatterns
+{
+    interface FlyBehavior
+    {
+        void fly();
+        
+    }
+    interface FlyWithWings : FlyBehavior
+    {
+        void fly();
+        //Implementa o voar dos patos
+    
+    }
+    interface FlyNoWay : FlyBehavior
+    {
+        void fly();
+        //fazer nda - não pode voar
+    }
+}
---
+++ HFDesignPatterns/HFDesignPatterns/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HFDesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public class Duck
+        {
+            QuackBehavior quackBehavior;
+            //mais
+            public void performQuack()
+            {
+                quackBehavior.quack();
+            }
+        }
+        public class MallardDuck : Duck
+        {
+            public MallardDuck()
+            {
+                quackBehavior = new Quack();
+                flyBehavior = new FlyWithWings();
+            }
+        }
+
+    }
+}
